1fd2b98e2f4b1bdcfadb93621c16618efaad853a
Bernard Duggan <bernard@hippware.com>
2018-07-27 16:57:25 +1000

    Subscription catchup support

--- lib/absinthe/phase/subscription/result.ex
+++ lib/absinthe/phase/subscription/result.ex
@@ -5,10 +5,39 @@ defmodule Absinthe.Phase.Subscription.Result do
   # subscription
 
   alias Absinthe.Blueprint
+  alias Absinthe.Blueprint.Continuation
 
   @spec run(any, Keyword.t()) :: {:ok, Blueprint.t()}
-  def run(blueprint, topic: topic) do
+  def run(blueprint, [topic: topic, catchup: catchup]) do
     result = %{"subscribed" => topic}
-    {:ok, put_in(blueprint.result, result)}
+    case catchup do
+      nil ->
+        {:ok, put_in(blueprint.result, result)}
+
+      fun when is_function(fun, 0) ->
+        acc =
+          blueprint.execution.acc
+          |> Map.put(:catchup_fun, fun)
+          |> Map.put(:topic, topic)
+
+        blueprint = put_in(blueprint.execution.acc, acc)
+
+        continuation = %Continuation{
+          phase_input: blueprint,
+          pipeline: [
+            Absinthe.Phase.Subscription.Catchup,
+          ]
+        }
+
+        result = Map.put(result, :continuation, [continuation])
+        {:ok, put_in(blueprint.result, result)}
+
+      val ->
+        raise """
+        Invalid catchup function. Must be a function of arity 0.
+
+        #{inspect(val)}
+        """
+    end
   end
 end
